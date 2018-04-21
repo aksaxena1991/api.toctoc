5afe2352f7799574a9ddbfe9afea3323b068a91b
Anubhav Saxena <tuthubpro@gmail.com>
2018-04-21 10:28:49 +0530

    first

---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var path = require('path');
+var connection = require('express-myconnection');
+var mysql = require('mysql');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var roles = require('./routes/roles');
+var auth = require('./routes/auth');
+var categories = require('./routes/categories');
+var products = require('./routes/products');
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    connection(mysql, {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'toctoc',
+        port: 3306
+    }, 'pool') //or single
+);
+// Authentication Start
+app.get('/api/auth/verify', auth.verify);
+app.get('/api/auth/login', auth.login);
+// Authentication End
+// Roles Start
+app.get('/api/roles/allRoles', roles.allRoles);
+app.post('/api/roles/addRole', roles.addRole);
+app.put('/api/roles/updateRole', roles.updateRole);
+app.delete('/api/roles/deleteRole', roles.deleteRole);
+// Roles End
+// Categories Start
+app.get('/api/categories/allCategories', categories.allCategories);
+app.get('/api/categories/allCategoriesByParentAndStoreId', categories.allCategoryByParentAndStoreId);
+app.get('/api/categories/allCategoriesByStoreId', categories.allCategoryByStoreId);
+app.post('/api/categories/addCategory', categories.addCategory);
+app.put('/api/categories/updateCategory', categories.updateCategory);
+app.delete('/api/categories/deleteCategory', categories.deleteCategory);
+// Categories End
+//Products Start
+app.get('/api/products/allProducts', products.allProducts);
+app.get('/api/products/allProductsByCategoryId', products.allProductsByCategoryId);
+app.put('/api/products/updateProduct', products.updateProduct);
+app.post('/api/products/addProduct', products.addProduct);
+app.delete('/api/products/deleteProduct', products.deleteProduct);
+// Products end
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+app.use(function (err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+module.exports = app;
---
+++ routes/roles.js
@@ -0,0 +1,131 @@
+exports.allRoles = function (req, res) {
+    try {
+        req.getConnection(function (err, connection) {
+            if (err) {
+                console.log("SQL Connection: ", err);
+            }
+            else {
+                connection.query("Select * from roles", function (error, rows) {
+                    if (error) {
+                        console.log("Error wile executing query ", error);
+                    }
+                    else {
+                        if (rows.length > 0) {
+                            var data = [];
+                            for (var index in rows) {
+                                var rowObj = rows[index];
+                                data.push(rowObj);
+                            }
+                            res.status(200).send({
+                                "code": 200,
+                                "message": "Roles are found.",
+                                "data": data
+                            });
+                        }
+                        else {
+                            res.status(404).send({
+                                "code": 404,
+                                "message": "Roles are not found.",
+                                "data": data
+                            });
+                        }
+
+                    }
+                });
+            }
+        });
+    } catch (ex) {
+        console.log("we caught an exception ", ex);
+    }
+
+}
+exports.addRole = function (req, res) {
+    try {
+        var requestParams = req.body;
+        req.getConnection(function (err, connection) {
+            if (err) {
+                console.log("SQL Connection: ", err);
+            }
+            else {
+                var query = "INSERT INTO roles SET ?";
+                var values = {
+                    role_name: requestParams.role_name
+                };
+                connection.query(query, values, function (error, rows) {
+                    if (error) {
+                        console.log("Error wile executing query ", error);
+                    }
+                    else {
+                        res.status(200).send({
+                            "code": 200,
+                            "message": "A new role has been added."
+                        });
+
+                    }
+                });
+            }
+        });
+    }
+    catch (ex) {
+        console.log("we caught an exception ", ex);
+    }
+}
+exports.updateRole = function (req, res) {
+    try {
+        var requestParams = req.body;
+
+        var role_id = requestParams.role_id;
+        var role_name = requestParams.role_name;
+        req.getConnection(function (err, connection) {
+            if (err) {
+                console.error('SQL Connection error: ', err);
+
+            } else {
+                connection.query('UPDATE roles SET role_name = ? WHERE role_id = ? order by timestamp', [role_name, role_id], function (error, result) {
+                    if (error) {
+                        console.log("Error wile executing query ", error);
+
+                    } else {
+                        res.status(200).send({
+                            "code": 200,
+                            "message": "A role has been updated."
+                        });
+
+                    }
+                });
+            }
+        });
+    }
+    catch (ex) {
+        console.log("we caught an exception ", ex);
+    }
+}
+exports.deleteRole = function (req, res) {
+    try {
+        var requestParams = req.body;
+
+        var role_id = requestParams.role_id;
+        req.getConnection(function (err, connection) {
+            if (err) {
+                console.error('SQL Connection error: ', err);
+
+            } else {
+                connection.query('DELETE from roles where role_id = ?', [role_id], function (error, result) {
+                    if (error) {
+                        console.log("Error wile executing query ", error);
+
+                    } else {
+                        res.status(200).send({
+                            "code": 200,
+                            "message": "A role has been deleted."
+                        });
+
+                    }
+                });
+            }
+        });
+    }
+    catch (ex) {
+        console.log("we caught an exception ", ex);
+    }
+}
